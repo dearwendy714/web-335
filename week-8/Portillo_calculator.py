fdf22f9c0c54193b7f4be8f0017a375698cd7188
dearwendy <dearwendy714@gmail.com>
2019-12-09 18:02:03 -0800

    complete assignment

---
+++ week-8/Portillo_calculator.py
@@ -0,0 +1,33 @@
+# ============================================
+# ; Title: Assignment 8.3
+# ; Author: Wendy Portillo
+# ; Date: 9 December 2019
+# ; Modified By: Wendy Portillo
+# ; Description: Demonstrates basic
+# ; python operation
+# ;===========================================
+
+# function that takes two parameters(numbers) and adds them together
+
+
+def add(x, y):
+    return x + y
+
+# function that takes two parameters(numbers) and finds the difference
+
+
+def subtract(x, y):
+    return x - y
+
+# function that takes two parameters(numbers) and finds the quotient of them
+
+
+def divide(x, y):
+    return x / y
+
+
+print(add(10, 10))
+
+print(subtract(10, 10))
+
+print(divide(10, 10))
---
+++ week-8/Portillo_hello_world.py
@@ -0,0 +1,9 @@
+# ============================================
+# ; Title: Assignment 8.2
+# ; Author: Wendy Portillo
+# ; Date: 9 December 2019
+# ; Modified By: Wendy Portillo
+# ; Description: Introduction to Anaconda
+# ;===========================================
+
+print('Hello World')
